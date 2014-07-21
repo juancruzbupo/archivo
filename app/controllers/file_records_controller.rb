492dbf28157bb564660514593c87a84a95fc4252
juancruzbupo <juancruzbupo@gmail.com>
2014-07-21 20:07:24 -0300

    before_action :set_file_record, only: [:show, :edit, :update, :destroy]

--- app/controllers/file_records_controller.rb
+++ app/controllers/file_records_controller.rb
@@ -1,66 +1,12 @@
-class FileRecordsController < ApplicationController
+class FileRecordsController < InheritedResources::Base
   before_action :set_file_record, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  # GET /file_records
-  # GET /file_records.json
-  def index
-    @file_records = FileRecord.all
-  end
 
   # GET /file_records/1
   # GET /file_records/1.json
   def show
     @file_records = FileRecord.find(set_file_record)
     @step = @file_record.steps.build
-  end
-
-  # GET /file_records/new
-  def new 
-    @file_record = FileRecord.new
-  end
-
-  # GET /file_records/1/edit
-  def edit
-  end
-
-  # POST /file_records
-  # POST /file_records.json
-  def create
-    @file_record = FileRecord.new(file_record_params)
-
-    respond_to do |format|
-      if @file_record.save
-        format.html { redirect_to @file_record, notice: 'File record was successfully created.' }
-        format.json { render :show, status: :created, location: @file_record }
-      else
-        format.html { render :new }
-        format.json { render json: @file_record.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /file_records/1
-  # PATCH/PUT /file_records/1.json
-  def update
-    respond_to do |format|
-      if @file_record.update(file_record_params)
-        format.html { redirect_to @file_record, notice: 'File record was successfully updated.' }
-        format.json { render :show, status: :ok, location: @file_record }
-      else
-        format.html { render :edit }
-        format.json { render json: @file_record.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /file_records/1
-  # DELETE /file_records/1.json
-  def destroy
-    @file_record.destroy
-    respond_to do |format|
-      format.html { redirect_to file_records_url, notice: 'File record was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
