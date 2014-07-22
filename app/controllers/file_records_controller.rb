b985b27a3191eb0e8b547690970b679a73552df6
juancruzbupo <juancruzbupo@gmail.com>
2014-07-22 18:44:16 -0300

    inherited_resources

--- app/controllers/file_records_controller.rb
+++ app/controllers/file_records_controller.rb
@@ -6,12 +6,6 @@ class FileRecordsController < InheritedResources::Base
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_file_record
-      @file_record = FileRecord.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def file_record_params
       params.require(:file_record).permit(:title)
     end
